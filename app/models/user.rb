b53bc26e051512d1bc4f4fbfd6b62b1d058b7fa5
Artem Shevchenko <ashev08@gmail.com>
2018-01-25 12:17:16 +0200

    Add name and lastname

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates: name, presence: true, length: {maximum: 50}
-  validates: lastname, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :lastname, presence: true, length: {maximum: 50}
 end
