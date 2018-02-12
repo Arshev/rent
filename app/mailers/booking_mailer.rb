c1d34a568b6672074bcb34887a90b3b812912202
Artem Shevchenko <ashev08@gmail.com>
2018-02-12 21:01:52 +0200

    Email notification on new bookings

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,2 +1,8 @@
 class BookingMailer < ApplicationMailer
+  default from: 'notifications@rent.com'
+
+  def new_booking_email
+    @booking = params[:booking]
+    mail(to: 'ashev08@gmail.com', subject: 'Новая заявка на бронирование')
+  end
 end
