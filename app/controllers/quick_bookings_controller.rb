db9ea23afabfd088e60ed80f121832c19fafaccc
Artem Shevchenko <ashev08@gmail.com>
2018-02-13 13:11:06 +0200

    Add twilio sms notification on quick booking

--- app/controllers/quick_bookings_controller.rb
+++ app/controllers/quick_bookings_controller.rb
@@ -3,6 +3,7 @@ class QuickBookingsController < ApplicationController
     @quick_booking = QuickBooking.create(quick_booking_params)
     if @quick_booking.save
       QuickBookingMailer.with(quick_booking: @quick_booking).new_quick_booking_email.deliver_later
+      @quick_booking.send_sms
       redirect_to root_path, notice: "Заявка успешно создана! Ожидайте звонка оператора."
     else
       redirect_to root_path, alert: "Что то пошло не так!"
@@ -12,6 +13,6 @@ class QuickBookingsController < ApplicationController
   private
 
     def quick_booking_params
-      params.require(:quick_booking).permit(:start_date, :end_date, :location, :name, :contacts, :is_sedan, :is_hatch, :is_kupe, :is_minivan, :is_universal, :start_time, :end_time)
+      params.require(:quick_booking).permit(:start_date, :end_date, :location, :name, :contacts, :is_sedan, :is_hatch, :is_cross, :is_minivan, :is_universal, :start_time, :end_time)
     end
 end
