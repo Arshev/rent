68c52e6f364d85e332bb2bf91cd112a4c4fef969
Artem Shevchenko <ashev08@gmail.com>
2018-03-19 18:42:32 +0200

    Show on main roles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @cars = Car.where(active: true).limit(9)
+    @cars = Car.where(active: true, show_on_main: true)
     @quick_booking = QuickBooking.new
     @reviews = Review.all.limit(9)
   end
