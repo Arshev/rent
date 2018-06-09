1bdf294a35de10649193fa74b831d586b66b32da
Artem Shevchenko <ashev08@gmail.com>
2018-06-09 15:32:26 +0200

    Fix js assets import for admin panel

--- app/assets/javascripts/admins_panel.js
+++ app/assets/javascripts/admins_panel.js
@@ -1,3 +1,9 @@
+//= require jquery
+//= require bootstrap-sprockets
 //= require admin/adminkit
 //= require admin/bootstrap.min
 //= require admin/jquery.min
+//= require rails-ujs
+//= require jquery-ui/datepicker
+//= require jquery-ui/i18n/datepicker-ru
+//= require turbolinks
