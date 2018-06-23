a9b5044bb1b043b87dec8c8d07056805bf51a25a
Artem Shevchenko <ashev08@gmail.com>
2018-06-23 08:30:21 +0200

    Fix Twilio problem with errors

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -23,16 +23,22 @@ class Booking < ApplicationRecord
   end
 
   def send_sms
-    if self.car.length < 3
-      @car_name = Car.where("id = '#{self.car}'").first.car_name
-    else
-      @car_name = self.car
+    begin
+      if self.car.length < 3
+        @car_name = Car.where("id = '#{self.car}'").first.car_name
+      else
+        @car_name = self.car
+      end
+      @client = Twilio::REST::Client.new
+      @client.messages.create(
+      from: '+13477089316',
+      to: '+79082900908',
+      body: "Новая заявка,#{self.firstname},#{self.lastname},#{@car_name},#{self.phone},#{self.start_date.strftime("%d-%m-%Y")},#{self.end_date.strftime("%d-%m-%Y")}"
+      )
+    return true
+    rescue Twilio::REST::RestError => error
+      @error = error
+      return false
     end
-    @client = Twilio::REST::Client.new
-    @client.messages.create(
-    from: '+13477089316',
-    to: '+79082900908',
-    body: "Новая заявка,#{self.firstname},#{self.lastname},#{@car_name},#{self.phone},#{self.start_date.strftime("%d-%m-%Y")},#{self.end_date.strftime("%d-%m-%Y")}"
-    )
   end
 end
