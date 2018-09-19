41732d8f820a6fdbe98f3f5f1825e7fce8a73d91
Artem Shevchenko <ashev08@gmail.com>
2018-09-19 10:45:36 +0200

    Redirects for admin in Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,21 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    if resource.is_admin?
+      admin_path # <- Path you want to redirect the user to after signup
+    else 
+      root_path
+    end
+  end
+  def after_sign_in_path_for(resource)
+    if current_user.is_admin?
+      admin_path # <- Path you want to redirect the user to after signup
+    else 
+      root_path
+    end
+  end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname])
