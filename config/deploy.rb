5f677df7c01d03c99801f8e2eadc329fa25815c7
Erosanin <Erosanin@MacBook-Pro.local>
2018-10-08 14:17:03 +0200

    npm install

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,10 +53,19 @@ namespace :deploy do
     on roles(:web) do
       within release_path do
         execute("cd #{release_path} && yarn install")
-        execute("cd #{release_path}/rent && rails webpacker:install")
       end
     end
   end
+end
+namespace :deploy do
+    desc 'Run rake npm install'
+    task :npm_install do
+      on roles(:web) do
+        within release_path do
+          execute("cd #{release_path} && npm install")
+        end
+      end
+    end
 end
 
 # namespace :deploy do
