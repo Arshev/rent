f009692d9afbaadb00d16af512b570531d7bb4e5
Erosanin <ashev08@gmail.com>
2020-03-12 11:07:44 +0200

    Try optimization

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,6 @@
 //= require admin/bootstrap.min
 //= require admin/jquery.min
 //= require admin/adminkit
-//= require rails-ujs
-//= require jquery-ui/datepicker
-//= require jquery-ui/i18n/datepicker-ru
 //= require turbolinks
 //= require_tree .
 
