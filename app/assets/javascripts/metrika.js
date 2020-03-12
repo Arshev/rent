e734a787b2e6a40aacccf77c4c83afa9f6a7db3e
Erosanin <ashev08@gmail.com>
2020-03-12 11:25:12 +0200

    try

--- app/assets/javascripts/metrika.js
+++ app/assets/javascripts/metrika.js
@@ -23,21 +23,9 @@ $( document ).on('turbolinks:load', function() {
             d.addEventListener("DOMContentLoaded", f, false);
         } else { f(); }
     })(document, window, "yandex_metrika_callbacks");
-    (function(w,d,u){
-        var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-        var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-    })(window,document,'https://cdn.bitrix24.ru/b11089610/crm/site_button/loader_2_hkhbwb.js');
-    // turbolinks reload
-    // $(document).on('turbolinks:before-visit', function () {
-    //     window.turbolinks_referer = location.href;
-    // });
-    
-    // $(document).on('turbolinks:load', function () {
-    //     if (window.turbolinks_referer) {
-    //         // yandex metrika
-    //         if (window.yaCounter48045371) {
-    //             window.yaCounter48045371.hit(location.href, $('title').html(), window.turbolinks_referer);
-    //         }
-    //     }
-    // });
+
+    // (function(w,d,u){
+    //     var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+    //     var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+    // })(window,document,'https://cdn.bitrix24.ru/b11089610/crm/site_button/loader_2_hkhbwb.js');
 })
