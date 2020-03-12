c4df4e2f7486e9971901ecdb8203da1563b2c0be
Erosanin <ashev08@gmail.com>
2020-03-12 18:31:22 +0200

    New chat

--- app/assets/javascripts/metrika.js
+++ app/assets/javascripts/metrika.js
@@ -34,8 +34,20 @@ $( document ).on('turbolinks:load', function() {
     //     } else { f(); }
     // })(document, window, "yandex_metrika_callbacks");
 
-    (function(w,d,u){
-        var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-        var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-    })(window,document,'https://cdn.bitrix24.ru/b11089610/crm/site_button/loader_2_hkhbwb.js');
+    // (function(w,d,u){
+    //     var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+    //     var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+    // })(window,document,'https://cdn.bitrix24.ru/b11089610/crm/site_button/loader_2_hkhbwb.js');
+    (function(d, w, m) {
+		window.supportAPIMethod = m;
+		var s = d.createElement('script');
+		s.type ='text/javascript'; s.id = 'supportScript'; s.charset = 'utf-8';
+		s.async = true;
+		var id = 'a5b9503208dc960c60af1b3fb77a08ef';
+		s.src = 'https://admin.verbox.ru/support/support.js?h='+id;
+		var sc = d.getElementsByTagName('script')[0];
+		w[m] = w[m] || function() { (w[m].q = w[m].q || []).push(arguments); };
+		if (sc) sc.parentNode.insertBefore(s, sc); 
+		else d.documentElement.firstChild.appendChild(s);
+	})(document, window, 'Verbox');
 })
