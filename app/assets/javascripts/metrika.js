c224bd24e446a6f7adfcb4a014f06facfef18623
Erosanin <ashev08@gmail.com>
2020-03-12 16:36:08 +0200

    new metrika counter

--- app/assets/javascripts/metrika.js
+++ app/assets/javascripts/metrika.js
@@ -1,28 +1,38 @@
 $( document ).on('turbolinks:load', function() {
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter48045371 = new Ya.Metrika({
-                    id:48045371,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                });
-            } catch(e) { }
-        });
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(48045371, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+    // (function (d, w, c) {
+    //     (w[c] = w[c] || []).push(function() {
+    //         try {
+    //             w.yaCounter48045371 = new Ya.Metrika({
+    //                 id:48045371,
+    //                 clickmap:true,
+    //                 trackLinks:true,
+    //                 accurateTrackBounce:true,
+    //                 webvisor:true
+    //             });
+    //         } catch(e) { }
+    //     });
     
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
+    //     var n = d.getElementsByTagName("script")[0],
+    //         s = d.createElement("script"),
+    //         f = function () { n.parentNode.insertBefore(s, n); };
+    //     s.type = "text/javascript";
+    //     s.async = true;
+    //     s.src = "https://mc.yandex.ru/metrika/watch.js";
     
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
+    //     if (w.opera == "[object Opera]") {
+    //         d.addEventListener("DOMContentLoaded", f, false);
+    //     } else { f(); }
+    // })(document, window, "yandex_metrika_callbacks");
 
     (function(w,d,u){
         var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
