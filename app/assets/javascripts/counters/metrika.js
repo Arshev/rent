c51ee4116d2e75a60f6cd1bd3c7f8757811b926d
Erosanin <ashev08@gmail.com>
2020-03-13 11:58:39 +0200

    Try

--- app/assets/javascripts/counters/metrika.js
+++ app/assets/javascripts/counters/metrika.js
@@ -19,19 +19,19 @@ $( document ).on('turbolinks:load', function() {
         accurateTrackBounce:true
     });
     
-    (function() { 
-        var s = document['createElement']('script');
-        s.type = 'text/javascript'; 
-        s.async = true; 
-        s.charset = 'utf-8';	
-        s.src = '//cleversite.ru/cleversite/widget_new.php?supercode=1&referer_main='+encodeURIComponent(document.referrer)+'&clid=58945FfebN&siteNew=77074'; 
-        var ss = document['getElementsByTagName']('script')[0]; 
-        if(ss) {
-            ss.parentNode.insertBefore(s, ss);
-        } else {
-            document.documentElement.firstChild.appendChild(s);
-        };
-    })(); 
+    // (function() { 
+    //     var s = document['createElement']('script');
+    //     s.type = 'text/javascript'; 
+    //     s.async = true; 
+    //     s.charset = 'utf-8';	
+    //     s.src = '//cleversite.ru/cleversite/widget_new.php?supercode=1&referer_main='+encodeURIComponent(document.referrer)+'&clid=58945FfebN&siteNew=77074'; 
+    //     var ss = document['getElementsByTagName']('script')[0]; 
+    //     if(ss) {
+    //         ss.parentNode.insertBefore(s, ss);
+    //     } else {
+    //         document.documentElement.firstChild.appendChild(s);
+    //     };
+    // })(); 
     // (function (d, w, c) {
     //     (w[c] = w[c] || []).push(function() {
     //         try {
