907e2add307b1cb35fd4bc07c912e6649bcc210d
Erosanin <ashev08@gmail.com>
2020-03-13 13:17:26 +0200

    try

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require counters/metrika
 //= require bootstrap-sprockets
 //= require admin/bootstrap.min
 //= require admin/jquery.min
