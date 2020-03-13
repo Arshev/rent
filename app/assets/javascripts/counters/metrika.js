e74c3bfd2cdfac98df8c0ae5752a8bbb8c59b613
Erosanin <ashev08@gmail.com>
2020-03-13 12:44:20 +0200

    Try

--- app/assets/javascripts/counters/metrika.js
+++ app/assets/javascripts/counters/metrika.js
@@ -9,6 +9,14 @@ $( document ).on('turbolinks:load', function() {
 //         accurateTrackBounce:true,
 //         webvisor:true
 //    });
+
+    (function(w,d,v3){
+    w.chaportConfig = {
+    appId : '5e6b639f70748130b6a7fd1e'
+    };
+
+    if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
+
     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
